5d9b7fbd690a11178b0602d9b8a53c950db36d77
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-04-16 11:10:58 +0700

    Implement two step verification

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 require_once __DIR__ . '/home.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/courses.php';
-require_once __DIR__ . '/roles.php';
-require_once __DIR__ . '/user.php';
-require_once __DIR__ . '/coursesManagementCategory.php';
-require_once __DIR__ . '/coursesManagementCourses.php';
-require_once __DIR__ . '/coursesManagementVideo.php';
-require_once __DIR__ . '/coursesManagementQuiz.php';
-require_once __DIR__ . '/coursesManagementQuizDetail.php';
-require_once __DIR__ . '/coursesManagementCoursesContent.php';
-require_once __DIR__ . '/coursesManagementCoursesContentItem.php';
-require_once __DIR__ . '/analytics.php';
-require_once __DIR__ . '/aiAnalytics.php';
+Route::middleware('twoStepVerification')->group(function () {
+    require_once __DIR__ . '/roles.php';
+    require_once __DIR__ . '/user.php';
+    require_once __DIR__ . '/coursesManagementCategory.php';
+    require_once __DIR__ . '/coursesManagementCourses.php';
+    require_once __DIR__ . '/coursesManagementVideo.php';
+    require_once __DIR__ . '/coursesManagementQuiz.php';
+    require_once __DIR__ . '/coursesManagementQuizDetail.php';
+    require_once __DIR__ . '/coursesManagementCoursesContent.php';
+    require_once __DIR__ . '/coursesManagementCoursesContentItem.php';
+    require_once __DIR__ . '/analytics.php';
+    require_once __DIR__ . '/aiAnalytics.php';
+});
